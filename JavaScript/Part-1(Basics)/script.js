4bf8f8b55d1dd41bf400edbd0f38f3f344975407
snehayadav278 <snehay851@gmail.com>
2026-02-07 17:53:40 +0000

    Basics

---
+++ JavaScript/Part-1(Basics)/script.js
@@ -0,0 +1,46 @@
+console.log("Hello World ! ");
+
+
+let a = 10;
+let b = 5;
+let sum = a + b;
+console.log(sum);
+console.log(typeof sum);
+
+
+let pencilPrice = 10;
+let eraserPrice = 5 ;
+console.log("The total price is : " , pencilPrice + eraserPrice , "Rupees");
+//second option (by using back tick)
+let output=`The total price is : ${pencilPrice + eraserPrice} Rupees ` ;
+console.log(output); 
+
+
+//operators
+
+//Arithmetic Operators
+let c = 10;
+let d = 5;
+console.log(c+d);
+console.log(c-d);
+console.log(c*d);
+console.log(c/d);
+console.log(c%d);
+
+//assignment operators
+
+//Unary Operators
+console.log(c++);
+console.log(++d);
+console.log(d--);
+
+//comparison operators
+let age = 17;
+console.log(age>18);
+console.log(age<18);
+console.log(age>=18);
+console.log(age == 17);
+console.log(age != 20);
+
+
+
---
+++ JavaScript/Part-2(Conditional Statements)/script.js
@@ -0,0 +1,28 @@
+//conditional statements
+
+//if statement
+let age1=23;
+if(age1>=18){
+    console.log("you can vote");
+}
+if(age1>20){
+    console.log("you are in your 20's");
+}
+
+//create a traffic light system that shows what to do based on color
+let color = "red" ;
+if(color == "red"){
+    console.log("Stop! light color is red .");
+}
+if(color == "yellow"){
+    console.log("slow down , light color is yellow . ");
+}
+if(color == "green"){
+    console.log("go , light color is green .");
+}
+
+//else if statement
+
+
+
+
